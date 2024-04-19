9899e211e550f126bb9386cab71e2311b521fa34
Feddis08 <feddis08@gmail.com>
2024-04-19 09:26:51 +0200

    the template

--- src/main/java/at/Feddis08/Main.java
+++
@@ -1,5 +0,0 @@
-package at.Feddis08;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/at/Feddis08/Template.java
@@ -0,0 +1,36 @@
+package at.Feddis08;
+
+import java.io.*;
+
+public class Template {
+
+    public static String savePath = "./res/";
+
+    public static String result = "";
+    public static String instruction = "";
+    public static void main(String[] args) throws IOException {
+        System.out.println(getFile(savePath + "./test.txt"));
+    }
+    private static void saveToFile(String filePath, String data) {
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter(filePath, "UTF-8");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+        writer.write(data);
+        writer.close();
+    }
+    public static String getFile(String path) throws IOException {
+        File file = new File(path);
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String st;
+        String result = br.readLine();
+        while ((st = br.readLine()) != null){
+            result = result + "\n" + st;
+        }
+        return result;
+    }
+}
